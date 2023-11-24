081e2f9f572d3f38942b7a8dde73a98ad691383f
zeVickingDev <eydaleine.patrice@orange.fr>
2023-11-24 17:42:39 +0100

    Page d'accueil

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,21 @@
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className='home'>
+      <section className='home__content'>
+        <h2>Bienvenue chez Ordi'hand</h2>
+        <h3>Vente d'ordinateurs d'occasion</h3>
+        <p>
+          N'hésitez pas à consulter nos différentes générations pour dénicher
+          l'occasion qui vous conviendra le plus.
+        </p>
+        <p>
+          Contactez nous pour réserver ou obtenir des informations sur nos
+          ordinateurs.
+        </p>
+      </section>
+      <section className='home__logo'>
+        <img src='logo.svg' alt='' className='logo' />
+      </section>
+    </div>
+  );
 }
