83a3724f0110821b6fc7077075090a2fa51d104f
zeVickingDev <eydaleine.patrice@orange.fr>
2023-12-03 18:47:22 +0100

    Maj page contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,15 @@
 export default function Contact() {
   return (
-    <section>
-      <h2>Nous contacter</h2>
-      <address>
-        <span>Tél : 06.79.63.14.30</span>
+    <div className='home'>
+      <section className='home__content'>
+        <h2>Nous contacter</h2>
         <br />
-        <span>Gujan-Mestras (33470)</span>
-      </address>
-    </section>
+        <address>
+          <span>Tél : 06.79.63.14.30</span>
+          <br />
+          <span>Gujan-Mestras (33470)</span>
+        </address>
+      </section>
+    </div>
   );
 }
